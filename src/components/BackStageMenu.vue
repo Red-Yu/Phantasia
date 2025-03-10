c99bffde17c815d62b8b30f78ae612d7b71ae1c3
WQH-Qing <berrytea0605@gmail.com>
2025-03-10 19:35:54 +0800

    後台MENU+MEMBER

--- src/components/BackStageMenu.vue
+++ src/components/BackStageMenu.vue
@@ -1,9 +1,68 @@
-<template>
-  <div>這是後台風琴選單</div>
+<script setup>
+import TabButton from "./BTN/TabButton.vue";
 
-  <!-- 子頁面顯示區域 -->
-  <main>
-    <router-view></router-view>
-    <!-- 顯示子頁面內容 -->
-  </main>
+
+const changeStyle = () => {};
+</script>
+
+<style scoped>
+@import "../Assets/css/main.css";
+@import "../Assets/css/backStageMenu.css";
+
+</style>
+
+
+  <!-- 後台左側風琴選單 -->
+<template>
+  <!-- sass:MenuSidebar -->
+  <div class="backStageMenu">
+    <div class="sidebar">
+      <div class="user">
+        <div class="useImg">
+          <div class="avatar"></div>
+        </div>
+      </div>
+      <div class="tab-group">
+        <TabButton
+          text="會員管理"
+          to="/BackstageMember"
+          @click="changeStyle"
+        />
+        <TabButton 
+          text="商品管理" 
+          to="/BackstageProduct" 
+          @click="changeStyle"
+        />
+        <TabButton 
+          text="資訊管理" 
+          to="/BackstageInfo" 
+          @click="changeStyle"
+        />
+        <div class="logout-container">
+          <button class="logout">
+            <div class=" btnKey-M dark">
+              <p>登出</p>
+              <div class="icon-M">
+                <div class="white-cross">
+                  <div class="cols">
+                    <span></span>
+                    <span></span>
+                  </div>
+                  <div class="rows">
+                    <span></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </button>
+        </div>
+      </div>
+    </div>
+  
+    <!-- 右側(子頁面)顯示區域 -->
+    <main>
+      <router-view></router-view>
+      <!-- 顯示子頁面內容 -->
+    </main>
+  </div>
 </template>
